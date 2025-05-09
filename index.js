5f07f75e3ca643f81ac72598ca75c0a78b62eb7e
m.kalmykov <m.kalmykov@itformat.site>
2025-05-09 12:06:02 +0300

    first logic

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 function addTask() {
-  let name = document.getElementById("taskName").value;
-  let container = document.querySelector(".tasks-container");
+    let name = document.getElementById("taskName").value;
+    let container = document.querySelector(".tasks-container");
 
+    let newTask = document.createElement("div");
+    newTask.classList.add("task__item");
 
-  container.innerHTML = '<div class="task__item">\n' +
-    '      <p class="task__item-title">' + name + '</p>\n' +
-    '      <div class="task__item_btns">\n' +
-    '        <button class="done" id="doneBtn">Заверишть</button>\n' +
-    '        <button class="delete" id="deleteBtn">Удалить</button>\n' +
-    '      </div>\n' +
-    '    </div>';
+    newTask.innerHTML = '<p class="task__item-title">' + name + '</p>\n' +
+        '      <div class="task__item_btns">\n' +
+        '        <button class="done" id="doneBtn">Заверишть</button>\n' +
+        '        <button class="delete" id="deleteBtn" onclick="removeTask(this)">Удалить</button>\n' +
+        '      </div>\n' +
+        '    </div>';
 
-    // document.body.insertBefore(newDiv, my_div);
+    container.insertBefore(newTask, container.querySelector(".task__item"));
 }
+
+function removeTask(rmBtn) {
+    console.log(rmBtn);
+    rmBtn.closest('.task__item').remove();
+}
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   // removeTask();
+// })
